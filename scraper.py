70dfcf51e9a7d508a9935bf55c163fe06103e662
Acropolis40 <arc.xoc@gmail.com>
2021-01-29 00:34:26 -0800

    Made code loop

--- scraper.py
+++ scraper.py
@@ -1,23 +1,26 @@
 import asyncio  #import asynchronous execution
 import pymongo
+import time, threading
+
 from pyppeteer import launch
 
+from datetime import timedelta #Used by timeloop to get the time
 
-async def capture(website, imgname):
-    browser = await launch()
-    page = await browser.newPage() #Create a new browser page
-    await page.goto(website)
-    element = await page.querySelector('h2')
-    headline = await page.evaluate('(element) => element.textContent', element) 
-    await browser.close() #close browser
-    print(headline)
-    return(headline)
+async def capture(website):
+    while(True):
+        browser = await launch()
+        page = await browser.newPage() #Create a new browser page
+        await page.goto(website)
+        element = await page.querySelector('h2')
+        headline = await page.evaluate('(element) => element.textContent', element) 
+        await browser.close() #close browser
+        print(headline)
+        await asyncio.sleep(15)
+
+def periodic_capture():
+    asyncio.get_event_loop().run_until_complete(capture('https://cnn.com'))
+    asyncio.get_event_loop().run_until_complete(capture('https://foxnews.com'))
 
 
-def main():
-    asyncio.get_event_loop().run_until_complete(capture('https://cnn.com', 'cnn.png'))
-    asyncio.get_event_loop().run_until_complete(capture('https://foxnews.com', 'fox.png'))
-
-  
-
-main()
+if __name__ == "__main__":
+    periodic_capture()
