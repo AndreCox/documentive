b90b903bac323b8032a92d42336936ede19346fe
Acropolis40 <arc.xoc@gmail.com>
2021-01-30 01:41:43 -0800

    Better Scraper Added Lots Of Changes

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
 import asyncio  #import asynchronous execution
-import time, threading
+import time
 import pymongo #allows connection to database
 from pyppeteer import launch #used for website scraping
 from datetime import datetime
+from threading import Event
 
-#############ENVIRONMENT VARIABLES########################
+############# ENVIRONMENT VARIABLES ########################
 
 DATABASE_NAME = "db"
 DATABASE_PORT = 27017
@@ -13,9 +14,13 @@ DATABASE_HOST = "mongodb://localhost/"
 DATABASE_USERNAME = "root"
 DATABASE_PASSWORD = "example"
 
-##########################################################
+WEBSITES = ['https://cnn.com', 'https://foxnews.com']
 
-#############Try to connect to database###################
+POLL_INTERVAL = 15
+
+############################################################
+
+############# Try to connect to database ###################
 
 try:
     myclient = pymongo.MongoClient( DATABASE_HOST, DATABASE_PORT, username = DATABASE_USERNAME, password = DATABASE_PASSWORD )
@@ -28,6 +33,10 @@ except Exception as e:
 db_archive = myclient["archive"]
 db_collection = db_archive["newsites"]
 
+####################################################################
+
+############### Scrape Website #######################################
+
 async def capture(website):
         browser = await launch()
         page = await browser.newPage() #Create a new browser page
@@ -35,24 +44,50 @@ async def capture(website):
         element = await page.querySelector('h2')
         headline = await page.evaluate('(element) => element.textContent', element) 
         await browser.close() #close browser
-        print("[+] " + headline)
         return(headline)
 
-def periodic_capture():
-    cnn = asyncio.get_event_loop().run_until_complete(capture('https://cnn.com'))
-    fox = asyncio.get_event_loop().run_until_complete(capture('https://foxnews.com'))
-    
-    store_cnn = {"Site": "CNN", "Headline": cnn, "Date": datetime.now()}
-    store_fox = {"Site": "FOX", "Headline": fox, "Date": datetime.now()}
-    
-    x = db_collection.insert_one(store_cnn)
-    x = db_collection.insert_one(store_fox)
+##################################################################################
 
-    time.sleep(15)
-    periodic_capture()
+##################### Handles database uploads ###################################
+
+def data_upload():
+    for  x in range(0, len(WEBSITES)):
+        headline = asyncio.get_event_loop().run_until_complete(capture(WEBSITES[x]))
+        site = WEBSITES[x].replace("https://","")
+        site = site.replace(".com", "")
+        site = site.upper()
+        try:
+            if (db_collection.find_one(skip=x)["Headline"] != headline):
+                print("[+] Headline Update For" + " " + site)
+                format_data = {"Site": site, "Headline": headline, "Date": datetime.now()}
+                db_collection.insert_one(format_data)
+            else:
+                print("[+] No Headline Update For" + " " + site)
+        except TypeError:
+            print("[+] No Database Exists, Creating New Database")
+            for  x in range(0, len(WEBSITES)):
+                headline = asyncio.get_event_loop().run_until_complete(capture(WEBSITES[x]))         
+                site = WEBSITES[x].replace("https://","")
+                site = site.replace(".com", "")
+                site = site.upper()
+                format_data = {"Site": site, "Headline": headline, "Date": datetime.now()}
+                db_collection.insert_one(format_data)
+            break
+
+def main():
+    try:
+        data_upload()
+        print("\n")
+        time.sleep(POLL_INTERVAL)
+    except KeyboardInterrupt:
+        print("[+] Exiting!")
+        exit()
 
 if __name__ == "__main__":
-    periodic_capture()
+    main()
+
+
+
 
 
 
