230682a4ae3336e3209ae73d6fb4de19fa7ba710
Acropolis40 <arc.xoc@gmail.com>
2021-01-30 18:05:43 -0800

    Fixed Breaking when multiple entries are added

--- scraper.py
+++ scraper.py
@@ -5,22 +5,22 @@ from pyppeteer import launch #used for website scraping
 from datetime import datetime
 from threading import Event
 
-############# ENVIRONMENT VARIABLES ########################
+############# ENVIRONMENT VARIABLES ###########################################################
 
 DATABASE_NAME = "db"
 DATABASE_PORT = 27017
-DATABASE_HOST = "mongodb://mongo/" #should be localhost if running localy if running in docker it should be mongo
+DATABASE_HOST = "mongodb://localhost/" #should be localhost if running localy if running in docker it should be mongo
 
 DATABASE_USERNAME = "root"
 DATABASE_PASSWORD = "example"
 
 WEBSITES = ['https://cnn.com', 'https://foxnews.com']
 
-POLL_INTERVAL = 15
+POLL_INTERVAL = 5 * 60
 
-############################################################
+###############################################################################################
 
-############# Try to connect to database ###################
+############# Try to connect to database ######################################################
 
 try:
     myclient = pymongo.MongoClient( DATABASE_HOST, DATABASE_PORT, username = DATABASE_USERNAME, password = DATABASE_PASSWORD )
@@ -33,9 +33,9 @@ except Exception as e:
 db_archive = myclient["archive"]
 db_collection = db_archive["newsites"]
 
-####################################################################
+################################################################################################
 
-############### Scrape Website #######################################
+############### Scrape Website #################################################################
 
 async def capture(website):
         browser = await launch(options={'args': ['--no-sandbox']})
@@ -46,33 +46,36 @@ async def capture(website):
         await browser.close() #close browser
         return(headline)
 
-##################################################################################
+###############################################################################################
 
-##################### Handles database uploads ###################################
+##################### Handles database uploads ################################################
+
+def check_existence(format_data):
+    for x in range(0, db_collection.count_documents({})):
+        if(db_collection.find_one(skip=x, sort=[( '_id', pymongo.DESCENDING )])["Site"] == format_data["Site"]):
+            if(db_collection.find_one(skip=x, sort=[( '_id', pymongo.DESCENDING )])["Headline"] == format_data["Headline"]):
+                return(True)
+            else:
+                return(False)
+    print("outer")
+    return(False)
 
 def data_upload():
+    print(db_collection.count_documents({}))
     for  x in range(0, len(WEBSITES)):
         headline = asyncio.get_event_loop().run_until_complete(capture(WEBSITES[x]))
         site = WEBSITES[x].replace("https://","")
         site = site.replace(".com", "")
         site = site.upper()
-        try:
-            if (db_collection.find_one(skip=x)["Headline"] != headline):
-                print("[+] Headline Update For" + " " + site)
-                format_data = {"Site": site, "Headline": headline, "Date": datetime.now()}
-                db_collection.insert_one(format_data)
-            else:
-                print("[+] No Headline Update For" + " " + site)
-        except TypeError:
-            print("[+] No Database Exists, Creating New Database")
-            for  x in range(0, len(WEBSITES)):
-                headline = asyncio.get_event_loop().run_until_complete(capture(WEBSITES[x]))         
-                site = WEBSITES[x].replace("https://","")
-                site = site.replace(".com", "")
-                site = site.upper()
-                format_data = {"Site": site, "Headline": headline, "Date": datetime.now()}
-                db_collection.insert_one(format_data)
-            break
+
+        format_data = {"Site": site, "Headline": headline, "Date": datetime.now()}
+        if (not check_existence(format_data)):
+            print("[+] Headline Update For" + " " + site)
+            db_collection.insert_one(format_data)
+        else:
+            print("[+] No Headline Update For" + " " + site)
+
+#############################################################################################
 
 def main():
     while(True):
